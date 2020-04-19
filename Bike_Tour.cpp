47eeedb8a27c9dfd2e1d90ff89715896c0ccbcd0
MOHD. FAIZAN <mdfaiz12343@gmail.com>
2020-04-19 15:54:50 +0530

    google_kickstart_B_1

---
+++ Bike_Tour.cpp
@@ -0,0 +1,51 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long int ll;
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+#define  boost ios_base::sync_with_stdio(false);cin.tie(NULL);
+#define ull unsigned long long
+#define d1(x) cout<<#x<<" "<<x<<endl;
+#define d2(x,y) cout<<#x<<" "<<x<<" "<<#y<<" "<<y<<endl;
+#define d2i(x,y,i) cout<<#x<<i<<" "<<x<<" "<<#y<<i<<" "<<y<<endl;
+
+#define fr(i,l,r) for(ll i=l;i<r;i++)
+#define mems(a,x) memset(a,x,sizeof(a))
+#define mod 1000000007
+#define ff first
+#define ss second
+#define pb(x) push_back(x)
+#define vll vector<ll>
+#define pbp(x,y) push_back(make_pair(x,y))
+#define  mat vector<vector<ll>>
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+void solve(){
+  ll n;
+  cin>>n;
+  ll c=0;
+  vll v(n);
+  fr(i,0,n)
+  cin>>v[i];
+  for(int i=1;i<n-1;i++){
+    if(v[i]>v[i-1] && v[i]>v[i+1])
+    c++;
+  }
+  cout<<c;
+
+}
+
+
+
+
+int main(){
+    ll t=1;
+  cin>>t;
+  ll f=1;
+    while(f<=t){
+        cout<<"Case #"<<f<<": ";
+        solve();
+        cout<<endl;
+        f++;
+    }
+    return 0;
+}
